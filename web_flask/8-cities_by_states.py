bf383f3b30a7efdc261e61d58fb65acfbb2f3d58
Krofah <amponsahkrofah@gmail.com>
2023-07-26 18:09:41 -0700

    task 0

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
-# File: 8-cities_by_states.py
-# Authors: Yoshua Lopez - Ma paz Quirola - Laura Socarras 
-# email: 
-
-""""
-Script starts Flask web app
-    listen on 0.0.0.0, port 5000
-    routes: /:
-            /cities_by_states:    Display HTML and state, city relations
+"""
+starts a Flask web application
 """
 
-from models import storage
-from models.state import State
 from flask import Flask, render_template
-
+from models import *
+from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """After each request remove current SQLAlchemy session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route("/cities_by_states")
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-       States/cities are sorted by name.
-    """
-    states = storage.all(State).values()
-    return render_template("8-cities_by_states.html", states=states)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
-# File: 9-states.py
-# Authors: Yoshua Lopez - Ma paz Quirola - Laura Socarras
-# email: 
-
-""""
-Script starts Flask web app
-    listen on 0.0.0.0, port 5000
-    routes: /:
-            /states/<id>:    Display HTML and state, city given state id
+"""
+starts a Flask web application
 """
 
-from models import storage
-from models.state import State
 from flask import Flask, render_template
-
+from models import *
+from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """After each request remove current SQLAlchemy session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states')
-@app.route('/states/<id>')
-def states_and_cities(id=None):
-    """Display html page; customize heading with state.name
-       fetch sorted cities for this state ID into LI tag ->in HTML file
-    """
-    states = storage.all(State).values()
-    if id is not None:
-        for state in states:
-            if state.id == id:
-                return(render_template("9-states.html", states=state))
-        return(render_template("9-states.html"))
-    return(render_template("9-states.html", states=states, variable=True))
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
